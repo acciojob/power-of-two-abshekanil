07eadf64661da9fc77f0d2d0c9c61cbbc229ba66
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-27 23:43:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const isPowerOfTwo = (number) => {
   //your code here
+	if(number === 0 || number < 0)
+	{
+		return false;
+	}
+
+	return Math.log2(number) % 1 === 0;
 };
 
 const num = parseInt(prompt("Enter a number"));
